44d0d4b01a0fe9d6d7114ca0b42543c13ebd00f8
Adrien Heyman <heyman591@hotmail.com>
2022-07-26 15:30:15 +0200

    fix test category, subcategory

--- app/src/main/java/filrougeaaa/Category.java
+++ app/src/main/java/filrougeaaa/Category.java
@@ -1,18 +1,74 @@
 package filrougeaaa;
+import java.sql.* ;
+
+import filrougeaaa.utils.DBManager;
 import filrougeaaa.utils.Model;
 public class Category extends Model{
     protected String name;
 
+	public Category(int id) {
+        try{
+            ResultSet resultat = DBManager.execute("SELECT * FROM category WHERE id_category = "+id) ;
+            if(resultat.next()){
+                this.name = resultat.getString("name") ;
+                this.id = id ;
+            }
+        }catch(SQLException ex) {
+            System.out.println("SQLException" + ex.getMessage());
+            System.out.println("SQLState" + ex.getSQLState());
+            System.out.println("VendorError"+ ex.getErrorCode());
+        }
+	}
+
 	@Override
 	public boolean get(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		try{
+            ResultSet resultat = DBManager.execute("SELECT * FROM category WHERE id_category = "+id);
+            if(resultat.next()){
+                this.name = resultat.getString("name");
+                this.id = id;
+                return true;
+            }
+        }
+        catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        return false;
 	}
 
 	@Override
 	public boolean save() {
-		// TODO Auto-generated method stub
-		return false;
+		String sql ;
+        if(this.id != 0){
+            sql = "UPDATE category SET name=? WHERE id_category = ?" ;
+        }else{
+            sql = "INSERT INTO category (name) VALUES (?)" ;
+        }
+        try {
+            PreparedStatement pstmt =  DBManager.conn.prepareStatement(sql) ;
+            pstmt.setString(1, this.name);
+            if(this.id != 0)
+                pstmt.setInt(2, this.id);
+            
+            pstmt.executeUpdate();
+			ResultSet keys = pstmt.getGeneratedKeys();
+			if(this.id == 0 && keys.next()){
+				this.id = keys.getInt(1);
+				return true;
+			}
+			else if(this.id != 0)
+				return true;
+			else
+				return false;
+        } catch (SQLException ex) {
+            System.out.println("SQLException" + ex.getMessage());
+            System.out.println("SQLState" + ex.getSQLState());
+            System.out.println("VendorError"+ ex.getErrorCode());
+            return false ;
+        }
 	}
 //#region Get/Set
 	public String getName() {
@@ -22,6 +78,13 @@ public class Category extends Model{
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getId(){
+		return this.id ;
+	}
+
+	public void setId(int id){
+		this.id = id ;
+	}
 //#endregion
-	
 }
--- app/src/main/java/filrougeaaa/SubCategory.java
+++ app/src/main/java/filrougeaaa/SubCategory.java
@@ -1,22 +1,80 @@
 package filrougeaaa;
 
+import java.sql.*;
+
+import filrougeaaa.utils.DBManager;
 import filrougeaaa.utils.Model;
 
 public class SubCategory extends Model{
     protected String name;
     protected Category category;
 
+	public SubCategory(int id) {
+		try{
+            ResultSet resultat = DBManager.execute("SELECT * FROM subcategory WHERE id_subcategory = "+id) ;
+            if(resultat.next()){
+                this.name = resultat.getString("name") ;
+				this.category.setId(resultat.getInt("id_category")) ;
+                this.id = id ;
+            }
+        }catch(SQLException ex) {
+            System.out.println("SQLException" + ex.getMessage());
+            System.out.println("SQLState" + ex.getSQLState());
+            System.out.println("VendorError"+ ex.getErrorCode());
+        }
+	}
 
 	@Override
 	public boolean get(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		try{
+            ResultSet resultat = DBManager.execute("SELECT * FROM subcategory WHERE id_subcategory = "+id);
+            if(resultat.next()){
+                this.name = resultat.getString("name");
+				this.category.setId(resultat.getInt("id_category"));
+                this.id = id;
+                return true;
+            }
+        }
+        catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        return false;
 	}
 
 	@Override
 	public boolean save() {
-		// TODO Auto-generated method stub
-		return false;
+		String sql ;
+        if(this.id != 0){
+            sql = "UPDATE subcategory SET name=?,id_category=? WHERE id_subcategorie = ?" ;
+        }else{
+            sql = "INSERT INTO subcategory (name,id_category) VALUES (?,?)" ;
+        }
+        try {
+            PreparedStatement pstmt =  DBManager.conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS) ;
+            pstmt.setString(1, this.name);
+            pstmt.setInt(2,this.category.getId());
+            if(this.id != 0)
+                pstmt.setInt(3, this.id);
+            
+            pstmt.executeUpdate();
+                ResultSet keys = pstmt.getGeneratedKeys();
+                if(this.id == 0 && keys.next()){
+                    this.id = keys.getInt(1);
+                    return true;
+                }
+                else if(this.id != 0)
+                    return true;
+                else
+                    return false;
+        } catch (SQLException ex) {
+            System.out.println("SQLException" + ex.getMessage());
+            System.out.println("SQLState" + ex.getSQLState());
+            System.out.println("VendorError"+ ex.getErrorCode());
+            return false ;
+        }
 	}
 
 	//#region get/set
@@ -34,6 +92,14 @@ public class SubCategory extends Model{
 
 	public void setCategory(Category category) {
 		this.category = category;
+	}
+
+	public int getId(int id){
+		return this.id ;
+	}
+
+	public void setId(int id){
+		this.id = id ;
 	}
 	//#endregion get/set
 	
