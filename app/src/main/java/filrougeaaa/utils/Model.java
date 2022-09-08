584967b38ecf403c9ef1e507eafa05b36a672731
Antony Alsberghe <antony.alsberghe@hotmail.fr>
2022-09-08 11:04:35 +0200

    Clean project

--- app/src/main/java/filrougeaaa/utils/Model.java
+++
@@ -1,9 +0,0 @@
-package filrougeaaa.utils;
-
-public abstract class Model {
-    public int id= 0;
-    public abstract boolean get(int id);
-    public abstract boolean get();
-    public abstract boolean save();
-    public abstract int getId();
-}
