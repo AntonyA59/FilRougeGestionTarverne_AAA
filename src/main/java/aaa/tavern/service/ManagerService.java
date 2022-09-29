ab3859fca8f4d2795c2763bfb9ec94a13f147621
Antony Alsberghe <Antony.alsberghe@hotmail.fr>
2022-09-29 22:19:47 +0200

    Test a refaire

--- src/main/java/aaa/tavern/service/ManagerService.java
+++ src/main/java/aaa/tavern/service/ManagerService.java
@@ -2,7 +2,6 @@ package aaa.tavern.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
@@ -11,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import aaa.tavern.dao.ManagerRepository;
+import aaa.tavern.dao.PlayerRepository;
 import aaa.tavern.dto.ManagerDto;
 import aaa.tavern.entity.Manager;
 import aaa.tavern.entity.Player;
@@ -19,59 +19,62 @@ import aaa.tavern.utils.ServiceUtil;
 
 @Service
 public class ManagerService {
-    
-    @Autowired
-    private ManagerRepository managerRepository;
 
-   
+	@Autowired
+	private ManagerRepository managerRepository;
 
+	private PlayerRepository playerRepository;
 
-    public Manager createManager(String name, Player player ) {
-        Manager manager = new Manager(name, 0, 100, 1, 0, player);
-        managerRepository.save(manager);
-        return manager;
-    }
+	//TODO: Refaire les tests
+	public Manager createManager(ManagerDto managerDto, int idPlayer) {
+		Player player = ServiceUtil.getEntity(playerRepository, idPlayer);
+		Manager manager = new Manager(managerDto.getName(), 0, 100, 1, 0, player);
+		managerRepository.save(manager);
+		return manager;
+	}
 
-    
-    public void deleteManager(Manager manager) {
-        Optional<Manager> managerOpt= managerRepository.findById(manager.getIdManager());
-        manager = managerOpt.get();
-        managerRepository.delete(manager);
-    }
+	public void deleteManager(Integer managerId) {
 
-    /**
-     * Convert a Manager class instance into a ManagerDto class instance
-     * @param manager
-     * @return ManagerDto
-     */
-    public ManagerDto loadManagerDto(Manager manager){
-        return new ManagerDto(manager);
-    }
+		managerRepository.deleteById(managerId);
+	}
 
-    /**
-     * Lists player's managers in the database and returns them in List of managerDto
-     * @param player
-     * @return List<ManagerDto>
-     */
-    public List<ManagerDto> listExistingManagerDto(Player player) {
-        List<Manager> managers = managerRepository.findByPlayer(player);
-        if(managers.isEmpty()){
-            throw new EntityNotFoundException();
-        }
-        List<ManagerDto> managersDto = new ArrayList<ManagerDto>();
-        for (Manager manager : managers) {
-            ManagerDto managerDto = loadManagerDto(manager);
-            managersDto.add(managerDto);
-        }
+	/**
+	 * Convert a Manager class instance into a ManagerDto class instance
+	 * 
+	 * @param manager
+	 * @return ManagerDto
+	 */
+	public ManagerDto loadManagerDto(int managerId) {
+		Manager manager = ServiceUtil.getEntity(managerRepository, managerId);
+		return new ManagerDto(manager);
+	}
 
-        return managersDto;
-    }
+	/**
+	 * Lists player's managers in the database and returns them in List of
+	 * managerDto
+	 * 
+	 * @param playerId
+	 * @return List<ManagerDto>
+	 */
+	public List<ManagerDto> listExistingManagerDto(int playerId) {
+		Player player = ServiceUtil.getEntity(playerRepository, playerId);
+		List<Manager> ListManagers = managerRepository.findByPlayer(player);
+		if (ListManagers.isEmpty()) {
+			throw new EntityNotFoundException();
+		}
+		List<ManagerDto> ListManagersDto = new ArrayList<ManagerDto>();
+		for (Manager manager : ListManagers) {
+			ManagerDto managerDto = loadManagerDto(manager.getIdManager());
+			ListManagersDto.add(managerDto);
+		}
 
-    @Transactional(rollbackOn = {EntityNotFoundException.class, ForbiddenException.class})
-    public Manager selectManager(int idManager){
-        Manager manager = ServiceUtil.getEntity(managerRepository, idManager);
+		return ListManagersDto;
+	}
 
-        return manager;
-    }
+	@Transactional(rollbackOn = { EntityNotFoundException.class, ForbiddenException.class })
+	public Manager selectManager(int idManager) {
+		Manager manager = ServiceUtil.getEntity(managerRepository, idManager);
+		return manager;
+	}
 
 }
--- src/test/java/aaa/tavern/service/ManagerServiceTest.java
+++ src/test/java/aaa/tavern/service/ManagerServiceTest.java
@@ -20,7 +20,9 @@ import aaa.tavern.dao.PlayerRepository;
 import aaa.tavern.dto.ManagerDto;
 import aaa.tavern.entity.Manager;
 import aaa.tavern.entity.Player;
+import aaa.tavern.entity.Recipe;
 import aaa.tavern.service.ManagerService;
+import aaa.tavern.utils.ServiceUtil;
 
 
 @SpringBootTest
@@ -40,24 +42,25 @@ public class ManagerServiceTest {
 
     @Test
     public void createManager() {
-        Manager manager = Mockito.mock(Manager.class);
-        Player player = Mockito.mock(Player.class);
-        manager = managerService.createManager("test", player);
-        Mockito.verify(managerRepository).save(manager);
+    	ManagerDto managerDto = Mockito.mock(ManagerDto.class);
+        
+        Mockito.when(managerService.createManager(managerDto, 0));
+        
     }
 
     @Test
     public void deleteManager() {
         Manager manager = Mockito.mock(Manager.class);
         Mockito.when(managerRepository.findById(0)).thenReturn(Optional.of(manager));
-        managerService.deleteManager(manager);
+        managerService.deleteManager(0);
         
-        Mockito.verify(managerRepository).delete(manager);
+        Mockito.verify(managerRepository).deleteById(0);
     }
 
     @Test
     public void listExistingManagerDto(){
         Player player = new Player();
+        playerRepository.save(player);
         Manager manager1 = new Manager("Test1", 10, 20, 20, 20, player);
         Manager manager2 = new Manager("Test2", 10, 20, 20, 20, player);
         Manager manager3 = new Manager("Test3", 10, 20, 20, 20, player);
@@ -65,8 +68,16 @@ public class ManagerServiceTest {
         managers.add(manager1);
         managers.add(manager2);
         managers.add(manager3);
-        Mockito.when(managerRepository.findByPlayer(player)).thenReturn(managers);
-        List<ManagerDto> managerDtos = managerService.listExistingManagerDto(player) ;
+        Optional<Player> optPlayer= Optional.of(player);
+        Mockito.when(playerRepository.findById(1)).thenReturn(optPlayer);
+        
+        
+        List<ManagerDto> managerDtos = managerService.listExistingManagerDto(1) ;
+
+        
+  
+        
+        
         
         assertEquals(managerDtos.size(), 3);
     }
