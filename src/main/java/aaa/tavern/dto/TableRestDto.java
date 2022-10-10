5b529442c19d61e7b6faf6c7fd49acb0f5066ef2
Antony Alsberghe <Antony.alsberghe@hotmail.fr>
2022-10-10 15:59:46 +0200

    TableRestRepository DAO Request findByPlace added and Test OK

--- src/main/java/aaa/tavern/dto/TableRestDto.java
+++ src/main/java/aaa/tavern/dto/TableRestDto.java
@@ -3,31 +3,56 @@ package aaa.tavern.dto;
 import aaa.tavern.entity.TableRest;
 
 public class TableRestDto {
-    
+
     private Integer id;
 
     private Integer numberPlace;
-    
+
     private Float hygiene;
-    
+
     private Float posX;
 
     private Float posY;
-    
+
     private Integer place;
 
-    public TableRestDto(TableRest tableRest){
+    public TableRestDto(TableRest tableRest) {
 
-        this.id=tableRest.getIdTable();
-        this.numberPlace=tableRest.getNumberPlace();
-        this.hygiene=tableRest.getHygiene();
-        this.posX=tableRest.getPosX();
-        this.posY=tableRest.getPosY();
-        this.place= tableRest.getPlace().getPlaceId();
+        this.id = tableRest.getIdTable();
+        this.numberPlace = tableRest.getNumberPlace();
+        this.hygiene = tableRest.getHygiene();
+        this.posX = tableRest.getPosX();
+        this.posY = tableRest.getPosY();
+        this.place = tableRest.getPlace().getPlaceId();
     }
 
-    //#region Get
-    
+    // #region Get
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TableRestDto other = (TableRestDto) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
     public Integer getNumberPlace() {
         return numberPlace;
     }
@@ -51,8 +76,6 @@ public class TableRestDto {
     public Integer getPlace() {
         return place;
     }
-    //#endregion
+    // #endregion
 
-
-    
 }
