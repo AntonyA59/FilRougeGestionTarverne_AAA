c1a84b07a5fd4ad14ccb031e85f597f89298add3
Antony <antony.alsberghe@hotmail.fr>
2022-10-18 16:33:44 +0200

    Modif CreateManagerDTO (test ok)

--- src/main/java/aaa/tavern/dto/CreateManagerDTO.java
+++ src/main/java/aaa/tavern/dto/CreateManagerDTO.java
@@ -1,7 +1,17 @@
 package aaa.tavern.dto;
 
+import aaa.tavern.entity.Manager;
+
 public class CreateManagerDTO {
     private String name;
+
+    protected CreateManagerDTO() {
+
+    }
+
+    public CreateManagerDTO(Manager manager) {
+        this.name = manager.getName();
+    }
 
     public String getName() {
         return name;
