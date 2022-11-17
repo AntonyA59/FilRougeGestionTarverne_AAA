5b6ae1119e773f1cb6d9e5b0c3239f09dfe1fdc7
Antony <antony.alsberghe@hotmail.fr>
2022-11-17 14:57:28 +0100

    modif loadManager's methods

--- src/main/java/aaa/tavern/service/RecipeCustomerService.java
+++ src/main/java/aaa/tavern/service/RecipeCustomerService.java
@@ -3,8 +3,6 @@ package aaa.tavern.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
