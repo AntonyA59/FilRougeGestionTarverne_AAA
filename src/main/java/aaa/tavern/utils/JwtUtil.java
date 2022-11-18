a39ce7f1f8fecd574f19d3ac6cd19beba01a6b52
Antony <antony.alsberghe@hotmail.fr>
2022-11-18 14:20:26 +0100

    prod modif

--- src/main/java/aaa/tavern/utils/JwtUtil.java
+++ src/main/java/aaa/tavern/utils/JwtUtil.java
@@ -3,7 +3,7 @@ package aaa.tavern.utils;
 public class JwtUtil {
     public static final String AUTH_HEADER = "Authorization";
     public static final String PREFIX = "Bearer ";
-    public static final long EXPIRE_ACCESS_TOKEN = 20 * 60 * 1000;
-    public static final long EXPIRE_REFRESH_TOKEN = 5 * 60 * 60 * 1000;
+    public static final long EXPIRE_ACCESS_TOKEN = 24 * 60 * 60 * 1000;
+    public static final long EXPIRE_REFRESH_TOKEN = 24 * 60 * 60 * 1000;
 
 }
